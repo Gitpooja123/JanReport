ebb227f24a183953a7dca9005183f4d871026cf1
KavyaGajula <kavyagajula1212@gmail.com>
2025-10-21 17:20:42 +0530

    Initial Setup

---
+++ Backend/src/main/java/MP/CIR/Backend/JanReportApplication.java
@@ -0,0 +1,13 @@
+package MP.CIR.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JanReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JanReportApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/MP/CIR/Backend/JanReportApplicationTests.java
@@ -0,0 +1,13 @@
+package MP.CIR.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JanReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
